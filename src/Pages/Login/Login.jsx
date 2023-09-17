24ee91aa26588dc102f53e77f858984b6cd67452
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2023-09-17 14:04:30 +0600

    firebase intigration

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,48 +1,62 @@
-import loginImage from '../../assets/images/login/login.svg'
+import loginImage from "../../assets/images/login/login.svg";
 
 const Login = () => {
+
+    const handleLogin = event =>{
+        event.preventDefault();
+    }
+
   return (
     <div>
       <div className=" py-16 bg-base-200">
-        <div className='pb-12'>
-        <h1 className="text-5xl text-center font-bold">Login now!</h1>
+        <div className="pb-12">
+          <h1 className="text-5xl text-center font-bold">Login now!</h1>
         </div>
-        
+
         <div className="grid grid-cols-2 space-x-5">
-          <div className=" flex justify-end">
-            <img src={loginImage} alt="" />
+          <div className=" flex justify-end ">
+            <img className="w-1/2" src={loginImage} alt="" />
           </div>
 
           <div className="card  w-full max-w-sm shadow-2xl bg-base-100">
             <div className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="email"
-                  className="input input-bordered"
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="password"
-                  className="input input-bordered"
-                />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">
-                    Forgot password?
-                  </a>
-                </label>
-              </div>
-              <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
-              </div>
+
+              <form onSubmit={}>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="email"
+                    name="email"
+                    className="input input-bordered"
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="password"
+                    placeholder="password"
+                    className="input input-bordered"
+                  />
+                  <label className="label">
+                    <a href="#" className="label-text-alt link link-hover">
+                      Forgot password?
+                    </a>
+                  </label>
+                </div>
+                <div className="form-control mt-6">
+                  <input
+                    className="btn btn-primary"
+                    type="submit"
+                    value="login"
+                  />
+                </div>
+              </form>
             </div>
           </div>
         </div>
